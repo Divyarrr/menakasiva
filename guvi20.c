83a64d8577e88260b87dcfabb910631c08ffa319
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-01-19 23:37:57 +0530

    Update guvi20.c

--- guvi20.c
+++ guvi20.c
@@ -3,11 +3,12 @@ int main()
 {
 	int a,i,k=0;
 	scanf("%d",&a);
-	for(i=1;i<=a;i++)
+	for(i=1;i<=5;i++)
 	{
-		k=i*5;
-		printf("%d",k);
+		k=i*a;
+		printf("%d\t",k);
 		k=1;
 		}
 		return 0;
 		}
+
