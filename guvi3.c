57fb15907f33bc17e9e854f2b9a31f9f6e82e1e0
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-01-20 00:23:40 +0530

    Update guvi3.c

--- guvi3.c
+++ guvi3.c
@@ -2,11 +2,16 @@
 int main()
 {
 	char ch;
+	int f=0;
 	scanf("%c",&ch);
-	if((ch>=48)&&(ch<=57))
-	printf("invalid");
-	else if((ch=='a')||(ch=='A')||(ch=='e')||(ch=='E')||(ch=='i')||(ch=='I')||(ch=='o')||(ch=='O')||(ch=='u')||(ch=='U'))
+ if((ch=='a')||(ch=='A')||(ch=='e')||(ch=='E')||(ch=='i')||(ch=='I')||(ch=='o')||(ch=='O')||(ch=='u')||(ch=='U'))
+ {
+ f++;
+ 	}
+ 	if(f>0)
 	printf("vowel");
+ 	else if((ch>9)&&(ch<65)&&(ch<97))
+ 	printf("invalid");
 	else
 	printf("consonant");
 	return 0;
