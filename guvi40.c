d471556c395c58939a000991432e14251631a6a1
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-01-21 00:44:07 +0530

    Create guvi40.c

---
+++ guvi40.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,d=0,t=1,t1=0,i;
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    {
+        d=t+t1;
+        printf("%d",d);
+        t=t1;
+        t1=d;
+        
+    }
+    return 0;
+}
+    
