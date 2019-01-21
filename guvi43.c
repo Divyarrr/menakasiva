6bb6439782245946c71afbf0f7213447494219fc
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-01-21 19:13:50 +0530

    Create guvi43.c

---
+++ guvi43.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+   char a[100],b[100];
+   int i,l=0,l1=0;
+   scanf("%s",&a);
+   scanf("%s",&b);
+   for(i=0;a[i]!='\0';i++)
+   {
+       printf("%s",a);
+       printf("%s",b);
+       break;
+   }
+      return 0;
+}
