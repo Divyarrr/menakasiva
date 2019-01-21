c76a22aa75cec190ec25ebe4d1052b14f995ee87
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-01-21 18:59:08 +0530

    Create guvi41.c

---
+++ guvi41.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+   char a[100];
+   int i,k;
+   scanf("%s",&a);
+   scanf("%d",&k);
+   for(i=0;i<k;i++)
+      {
+        printf("%s\n",a);
+      }
+   return 0;
+}
