01204206f3a22bd50b460b207bc805cea6ea5215
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-01-21 18:27:29 +0530

    Update guvi31.c

--- guvi31.c
+++ guvi31.c
@@ -5,8 +5,10 @@ int main() {
    int i,l=0;
    scanf("%[^\n]s",&a);
    for(i=0;a[i]!='\0';i++)
-   if((a[i]>=65)&&(a[i]<=91)||(a[i]>=97)&&(a[i]<=122))
-       l++;
+      {
+          if(a[i]>' ')
+          l++;
+      }
    printf("%d",l);
    return 0;
 }
