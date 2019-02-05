8554c8efc9772094a59d8ebf748f01cbafc28a35
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-02-05 17:47:44 +0530

    Create guvi81.c

---
+++ guvi81.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+   int kabalininja,opponentninja;
+   scanf("%d%d",&kabalininja,&opponentninja);
+   if(kabalininja<opponentninja)
+   {
+       printf("%d",opponentninja-kabalininja);
+   }
+   return 0;
+}
+      
+   
