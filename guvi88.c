f4b042106f1dad110c35c75768801347c58e93f5
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-02-05 19:48:27 +0530

    Create guvi88.c

---
+++ guvi88.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() {
+   int N,M,i,gcd=0;
+   scanf("%d%d",&N,&M);
+   if(N<M)
+   {
+   for(i=1;i<=M;i++)
+   {
+       if((N%i==0)&&(M%i==0))
+       gcd=i;
+   }
+   }
+   else
+   for(i=1;i<=N;i++)
+   {
+       if((N%i==0)&&(M%i==0))
+       gcd=i;
+   }
+   printf("%d",((N*M)/gcd));
+   return 0;
+}
+   
+   
