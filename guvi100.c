01ed998bcdfe08f94a2d58c96062b4df2e7baf1b
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-02-05 21:24:49 +0530

    Create guvi100.c

---
+++ guvi100.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+   int N,i,sum=0,rem=0;
+   scanf("%d",&N);
+   while(N)
+   {
+      rem=N%10;
+      sum=sum+rem;
+      N=N/10;
+   }
+   printf("%d",sum);
+return 0;
+}
+   
