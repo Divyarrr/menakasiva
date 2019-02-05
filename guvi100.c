de71b43ce58afd087be9c2102ca701bad9fe6337
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-02-05 21:28:18 +0530

    Update guvi100.c

--- guvi100.c
+++ guvi100.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int main() {
-   int N,i,sum=0,rem=0;
+   int N,i,sum=1,rem=0;
    scanf("%d",&N);
    while(N)
    {
       rem=N%10;
-      sum=sum+rem;
+      sum=sum*rem;
       N=N/10;
    }
    printf("%d",sum);
