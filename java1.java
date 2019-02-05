c073a95c8c71aa4a4a6a2a774e0a757161b38117
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-02-05 11:32:10 +0530

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,11 @@
+Java.util.scanner;
+class input
+{
+    public static void main(string[]args)
+    {
+        Scanner input=new Scanner(system.in)
+        System.out.println("enter an int");
+        int number=input.nextInt();
+        System.out.println("number is"+number);
+    }
+}
