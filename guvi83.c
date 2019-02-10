f60d819b4d2bb1814a5bf3b795db16c73dfe0b11
menaka555 <45939359+menaka555@users.noreply.github.com>
2019-02-10 20:44:09 +0530

    Update guvi83.c

--- guvi83.c
+++ guvi83.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
-
-int main() {
+int main()
+{
+   int a;
    char c;
-   int a,b;
-   scanf("%c",&c);
-   scanf("%d%d",&a,&b);
+   int b;
+   scanf("%d%c%d",&a,&c,&b);
    if(c=='/')
-   printf("%d",a/b);
-   else if(c=='%')
-   printf("%d",a%b);
+   {
+       printf("%d",a/b);
+   }
+   if(c=='%')
+   {
+       printf("%d",a%b);
+   }
    return 0;
 }
+
